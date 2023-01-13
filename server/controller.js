0f4e76589330f4389f6e4f4bf025ae6d457c1980
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-01-12 19:24:52 -0700

    adding in code for fortune

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,16 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["Bide your time, for success is near.", "Embrace this love relationship you have", "I learn by going where I have to go.", "It’s time to get moving. Your spirits will lift accordingly.", "Technology is the art of arranging the world so we do not notice it."];
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
     }
 
 }
--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 const { getCompliment } = require('./controller')
+const { getFortune } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
+app.get("/api/compliment", getFortune);
 
 app.listen(4000, () => console.log("Server running on 4000"));
